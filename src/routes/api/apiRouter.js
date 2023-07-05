717836818310cffa83913dddc19bed053c5617be
timlohrer <me@timlohrer.de>
2023-07-05 16:21:59 +0000

    add 200 response on /api for uptime tracker

--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -4,4 +4,6 @@ const apiRouter = Router();
 apiRouter.use('/groups', require('./groups/groupsRouter'));
 apiRouter.use('/devices', require('./devices/devicesRouter'));
 
+apiRouter.get('/', (req, res) => res.sendStatus(200))
+
 module.exports = apiRouter;
